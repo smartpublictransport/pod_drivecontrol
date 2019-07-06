3eb154c532b01c8d1d1e14b7f8bd9b7116ecaa7c
a7kntr <albert_tpdn@hotmail.com>
2019-07-06 13:07:45 -0400

    First commit for location files

---
+++ location.cpp
@@ -0,0 +1,7 @@
+#include "location.h"
+
+Location::Location(int longitude, int latitude)
+{
+    m_longitude =  longitude;
+    m_latitude = latitude;
+}
---
+++ location.h
@@ -0,0 +1,14 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+class Location
+{
+public:
+    Location (int longitude, int latitude);
+
+private:
+    int m_longitude;
+    int m_latitude;
+};
+
+#endif
